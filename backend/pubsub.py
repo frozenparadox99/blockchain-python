7007b97d5580a76e1ef00af3cd43e92458c357b3
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-04-02 15:46:36 +0530

    broadcast transactions using pubsub

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -4,6 +4,8 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 
 from backend.blockchain.block import Block
+from backend.wallet.transaction import Transaction
+from backend.wallet.transaction_pool import TransactionPool
 
 subscribe_key='sub-c-a00f7070-8fac-11eb-bfcb-9a3fb4a80a34'
 publish_key='pub-c-55a72f27-08e8-4ed5-91d0-f2886f06118a'
@@ -14,12 +16,14 @@ pnconfig.publish_key = publish_key
 
 CHANNELS = {
     'TEST':'TEST',
-    'BLOCK':'BLOCK'
+    'BLOCK':'BLOCK',
+    'TRANSACTION':'TRANSACTION'
 }
 
 class Listener(SubscribeCallback):
-    def __init__(self,blockchain):
+    def __init__(self,blockchain,transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
 
     def message(self,pubnub,message_object):
@@ -36,6 +40,13 @@ class Listener(SubscribeCallback):
             except Exception as e:
                 print(f'\n -- Did not replace chain: {e}')
 
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print('\n -- Set the new transaction in the transaction pool')
+
+
+
 
 
 class PubSub():
@@ -43,10 +54,10 @@ class PubSub():
     Handles the publish/subscribe layer of the app
     Provides comms bw the nodes of the blockchain network
     """
-    def __init__(self,blockchain):
+    def __init__(self,blockchain,transaction_pool):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain,transaction_pool))
     
     def publish(self,channel,message):
         """
@@ -59,6 +70,12 @@ class PubSub():
         Broadcast a block object to all nodes
         """
         self.publish(CHANNELS['BLOCK'],block.to_json())
+
+    def broadcast_transaction(self,transaction):
+        """
+        Broadcast a transaction object to all nodes
+        """
+        self.publish(CHANNELS['TRANSACTION'],transaction.to_json())
 
 
 def main():
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -7,14 +7,14 @@ class Transaction:
     """
     Document of an exchange in currency from a sender to one or more recipients.
     """
-    def __init__(self,sender_wallet,recipient,amount):
-        self.id = str(uuid.uuid4())[0:8]
-        self.output = self.create_output(
+    def __init__(self,sender_wallet=None,recipient=None,amount=None, id=None, output=None, input=None):
+        self.id = id or str(uuid.uuid4())[0:8]
+        self.output = output or self.create_output(
             sender_wallet,
             recipient,
             amount
         )
-        self.input = self.create_input(sender_wallet,self.output)
+        self.input = input or self.create_input(sender_wallet,self.output)
 
     def create_output(self,sender_wallet,recipient,amount):
         """
@@ -64,6 +64,14 @@ class Transaction:
         """
 
         return self.__dict__
+    
+    @staticmethod
+    def from_json(transaction_json):
+        """
+        Deserialize the transaction's json rep back into a transaction instance
+        """
+        return Transaction(**transaction_json)
+
 
     @staticmethod
     def is_valid_transaction(transaction):
@@ -87,6 +95,13 @@ class Transaction:
 def main():
     transaction=Transaction(Wallet(),'recipient',15)
     print(f'transactipon : {transaction.__dict__}')
+
+    transaction_json = transaction.to_json()
+
+    restored_transaction = Transaction.from_json(transaction_json)
+    print(f'\nrestored_transaction : {restored_transaction.__dict__}')
+
+
 
 if __name__ == "__main__":
     main()
