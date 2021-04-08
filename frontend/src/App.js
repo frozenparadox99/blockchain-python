f9a545b001a01cb2b68dabdd14484f19999e590a
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-04-08 12:53:10 +0530

    react hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-
+import React, {useState} from "react"
 
 function App() {
+
+  const [userQuery,setUserQuery]  = useState('')
+
+  const updateUserQuery = (event) =>{
+    console.log(userQuery)
+    setUserQuery(event.target.value)
+  }
+
+  const searchQuery = ()=>{
+    window.open(`https://google.com/search?q=${userQuery}`)
+  }
+
   return (
-    <div className="App" >Hello World</div>
+    <div className="App" >
+      <input value={userQuery} onChange={updateUserQuery} />
+      <button onClick={searchQuery} >Search</button>
+    </div>
   );
 }
 
