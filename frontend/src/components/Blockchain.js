5433f59b04bfc2abf197a842e0736efd10d70402
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-04-11 17:37:56 +0530

    added pagination

--- frontend/src/components/Blockchain.js
+++ frontend/src/components/Blockchain.js
@@ -1,22 +1,54 @@
 import React,{useState, useEffect} from "react"
 import Block from "./Block"
 import {API_BASE_URL} from "../config"
+import { Button } from "react-bootstrap"
+
+const PAGE_RANGE = 3
 
 function Blockchain(){
     const [blockchain,setBlockChain] = useState([])
+    const [blockchainLength, setBlockchainLength] = useState(0)
 
-    useEffect(()=>{
-        fetch(`${API_BASE_URL}/blockchain`)
+    const fetchBlockchainPage = ({start,end})=>{
+        fetch(`${API_BASE_URL}/blockchain/range?start=${start}&end=${end}`)
         .then(response=>response.json())
         .then(json=>setBlockChain(json))
-    
+    }
+
+    useEffect(()=>{
+        fetchBlockchainPage({start:0,end:PAGE_RANGE})
+        
+        fetch(`${API_BASE_URL}/blockchain/length`)
+        .then(response=>response.json())
+        .then(json=>setBlockchainLength(json))
+
       },[])
+
+      const buttonNumbers = []
+      for(let i=0;i<blockchainLength/PAGE_RANGE;i++){
+          buttonNumbers.push(i)
+      }
 
       return (
           <div className="Blockchain" >
               <h3>Blockchain</h3>
             <div>
       {blockchain.map((block)=><div key={block.hash} >{<Block key={block.hash} block={block} />}</div>)}
+            </div>
+            <div>
+                {
+                    buttonNumbers.map(number=>{
+                        const start = number*PAGE_RANGE
+                        const end = (number+1)*PAGE_RANGE
+                        return (
+                            <spam key={number} onClick={()=>fetchBlockchainPage({start,end})} >
+                                <Button size="sm" variant="danger" >
+                                    {number+1}
+                                </Button>{" "}
+                            </spam>
+                        )
+                    })
+                }
             </div>
           </div>
       )
